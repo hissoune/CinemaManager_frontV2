e8ab992b5dc19105f1dcf402e8d1c5c25255c2e7
hissoune <khalidhissoune962@gmail.com>
2024-10-17 06:51:00 +0100

    Fix : fix the disolayin of the movies slayder

--- src/Hooks/useMoviesClient.jsx
+++ src/Hooks/useMoviesClient.jsx
@@ -2,35 +2,34 @@ import { useEffect, useState } from 'react';
 import axiosInstance from '../client/axiosInstance';
 
 function useMoviesClient() {
-   const [movies , setMovies] = useState();
+   const [movies , setMovies] = useState([]);
    const [movisLoading,setMovisLoading]=useState(false);
    const [error,setError]=useState(null);
 
+
+
+   const getmovies = async ()=>{
+        
+    setMovisLoading(true);
+        try {
+
+            const response = await axiosInstance.get('/public/movies');
+      
+        
+        setMovies(response.data);
+console.log(movies);
+
+        } catch (error) {
+            setError(error.message);
+        } finally {
+            setMovisLoading(false);
+        }
+    };
+
       useEffect(()=>{
-
-
-      const getmovies =async ()=>{
-        
-        setMovisLoading(true);
-            try {
-
-                const response = await axiosInstance.get('/public/movies');
-
-                setMovies(response.data);
-
-            } catch (error) {
-                setError(error.message);
-            } finally {
-                setMovisLoading(false);
-            }
-        };
-
         getmovies();
-        console.log(movies);
         
-      }
-
-      )
+      },[])
 
   return {
     movies,
--- src/Pages/Client/Home.jsx
+++ src/Pages/Client/Home.jsx
@@ -1,8 +1,43 @@
+import MoviesSlider from "../../Components/slyders/MoveisSlider"
 
 function Home() {
   return (
     <div className="bg-[#181823] h-screen">
-      
+          <>
+      <div className="relative w-full h-[50vh]">
+       
+
+        
+           <div className="h-full w-full bg-cover bg-center" style={{ backgroundImage: 'url(/public/cinema-movie-night-3d-vector-clapper-board-director-s-megaphone-ticket-3d-glasses-movie-tape_606402-430.jpg)' }}>
+  <div className="flex items-center justify-center h-full bg-black bg-opacity-50">
+    <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold text-white tracking-wide uppercase">
+      Experience the Magic of Cinema
+    </h1>
+  </div>
+</div>
+
+
+        
+      </div>
+
+      <div className="min-h-[80vh] bg-gray-900 flex flex-col items-center justify-center">
+        <h1 className="text-center text-4xl md:text-5xl lg:text-6xl text-white font-serif font-bold mb-8">
+          This Week
+        </h1>
+        {/* <div className="w-full">
+          <SessionsSlayder />
+        </div> */}
+      </div>
+
+      <div className="h-auto bg-gray-800 flex flex-col items-center justify-center py-10">
+        <h1 className="text-center text-4xl md:text-5xl lg:text-6xl text-white font-serif font-bold mb-8">
+          Movies
+        </h1>
+        <div className="w-full">
+          <MoviesSlider />
+        </div>
+      </div>
+    </>
     </div>
   )
 }
