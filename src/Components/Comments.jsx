4b5863c884041896f8daf27e1a0be0a278148571
hissoune <khalidhissoune962@gmail.com>
2024-10-22 17:24:29 +0100

    Fix : fixing the comments displaying and display the date

--- src/Components/Comments.jsx
+++ src/Components/Comments.jsx
@@ -30,18 +30,56 @@ export default function Comments({movieId}) {
     }
     if(error) return <div>{error.msg}</div>
       return (
-        <section className="bg-white dark:bg-gray-900 py-8 lg:py-16 antialiased  h-full ">
+        <section className="py-8 lg:py-16 antialiased  h-full ">
           <div className="max-w-2xl mx-auto px-4">
             <div className="flex justify-between items-center mb-6">
               <h2 className="text-lg lg:text-2xl font-bold text-gray-900 dark:text-white">Comments ({comments.length})</h2>
             </div>
+
+            {comments.length > 0 ? (
+              comments.map((comment) => (
+                <article key={comment.id} className="p-6 text-base  mb-3">
+                  <footer className="flex justify-between items-center mb-2">
+                    <div className="flex items-center">
+                      <p className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
+                        <img className="mr-2 w-6 h-6 rounded-full" src={comment.user?.image} alt={comment.user?.name} />
+                        {comment.user?.name}
+                      </p>
+                      <p className="text-sm text-gray-600 dark:text-gray-400">
+                        <time>{new Date(comment.createdAt).toLocaleString()}</time>
+                      </p>
+                    </div>
+                    <button
+                      className="inline-flex items-center p-2 text-2xl font-medium text-gray-500 dark:text-gray-400  focus:ring-4"
+                      type="button"
+                    >
+                      <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
+                        <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
+                      </svg>
+                      <span className="sr-only">Comment settings</span>
+                    </button>
+                  </footer>
+                  <p className="text-gray-500 dark:text-gray-400">{comment.content}</p>
+                  <div className="flex items-center mt-4 space-x-4">
+                    <button type="button" className="text-sm text-gray-500 dark:text-gray-400 hover:underline">
+                      Reply
+                    </button>
+                  </div>
+                </article>
+              ))
+            ) : (
+              <div className="p-6 text-base bg-white rounded-lg dark:bg-gray-900 mb-3 text-center text-gray-500 dark:text-gray-400">
+                Be the first one to comment!
+              </div>
+            )}
+
     
             <form onSubmit={handleCommentSubmit} className="mb-6">
               <div className="py-2 px-4 mb-4 bg-white rounded-lg border  dark:bg-gray-800 dark:border-gray-700">
                 <input type="text"hidden  name="movieId" value={formData?.movieId || movieId} onChange={onChange} />
                 <textarea
                   id="comment"
-                  rows="6"
+                  rows="4"
                   name="content"
                   className="w-full text-sm text-gray-900 border-0 outline-0 focus:ring-0 dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
                   placeholder="Write a comment..."
@@ -58,36 +96,7 @@ export default function Comments({movieId}) {
               </button>
             </form>
     
-            {comments.map((comment) => (
-              <article key={comment.id} className="p-6 text-base bg-white rounded-lg dark:bg-gray-900 mb-3">
-                <footer className="flex justify-between items-center mb-2">
-                  <div className="flex items-center">
-                    <p className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
-                      <img className="mr-2 w-6 h-6 rounded-full" src={comment.user?.image} alt={comment.user?.name} />
-                      {comment.user?.name}
-                    </p>
-                    <p className="text-sm text-gray-600 dark:text-gray-400">
-                      <time >{comment.date}</time>
-                    </p>
-                  </div>
-                  <button
-                    className="inline-flex items-center p-2 text-sm font-medium text-gray-500 dark:text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 dark:bg-gray-900 dark:hover:bg-gray-700"
-                    type="button"
-                  >
-                    <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
-                      <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
-                    </svg>
-                    <span className="sr-only">Comment settings</span>
-                  </button>
-                </footer>
-                <p className="text-gray-500 dark:text-gray-400">{comment.content}</p>
-                <div className="flex items-center mt-4 space-x-4">
-                  <button type="button" className="text-sm text-gray-500 dark:text-gray-400 hover:underline">
-                    Reply
-                  </button>
-                </div>
-              </article>
-            ))}
+           
           </div>
         </section>
       );
