f3060dd3831a86cad244e5d04ab3cf4a01a0af01
hissoune <khalidhissoune962@gmail.com>
2024-10-22 11:40:50 +0100

    Fix : removing strict mod

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
@@ -6,9 +5,7 @@ import AuthProvider from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
 
-  <StrictMode>
     <AuthProvider>
        <App />
     </AuthProvider>
-  </StrictMode>,
 )
