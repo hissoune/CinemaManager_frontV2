92ac40630c4604f5f3609d19d2286c376d6e8708
hissoune <khalidhissoune962@gmail.com>
2024-10-24 23:57:27 +0100

    Fix fix the dash layout for the responsive

--- src/Pages/Admin/Dashboard.jsx
+++ src/Pages/Admin/Dashboard.jsx
@@ -4,10 +4,10 @@ import Sidebar from "../../Components/Sidebar";
 export default function Dashboard() {
   return (
     <div className="bg-gray-600 grid grid-cols-12 gap-1 ">
-    <div className={`col-span-2 h-screen`}>
+    <div className={`lg:col-span-2 lg:h-screen`}>
         <Sidebar/>
         </div>
-    <div className="col-span-10">
+    <div className="col-span-12 sm:h-screen lg:col-span-10">
             <Outlet/>
     </div>
 </div>
