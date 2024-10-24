d751a5bc4e304cca869b5068ad8a568bd68d0ed9
hissoune <khalidhissoune962@gmail.com>
2024-10-24 23:55:53 +0100

    Fix : fis the sidebar for th eresponsive and toogle

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,76 +1,121 @@
-import { Link, useNavigate } from "react-router-dom"
-import {  useAuthContext } from "../context/AuthContext";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuthContext } from "../context/AuthContext";
+import { FiMenu, FiX } from "react-icons/fi"; // Burger and close icons
+import { FaFilm, FaDoorOpen, FaHome, FaUsers, FaRegCalendarAlt, FaChair } from "react-icons/fa"; // Sidebar icons
 
 function Sidebar() {
-    const {  logout } = useAuthContext();
-    const navigate = useNavigate();
+  const { logout } = useAuthContext();
+  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
 
-    const handleLogout = () => {
-        logout();
-        navigate('/');
+  const handleLogout = () => {
+    logout();
+    navigate("/");
+    setIsOpen(false);
+  };
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen); 
+  };
+
+  const handleLinkClick = () => {
+    setIsOpen(false); 
   };
 
   return (
-    
-  <div className="py-4 fixed overflow-y-auto  bg-[#181823] h-screen p-6">
-      <ul className="space-y-2 font-medium">
-         <li>
-            <Link to="/dashboard" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-               <svg className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
-                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
-                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
-               </svg>
-               <span className="ms-3">Dashboard</span>
-            </Link>
-         </li>
-         <li>
-           <Link to="/dashboard/movise-admin" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-               <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                  <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z"/>
-               </svg>
-               <span className="flex-1 ms-3 whitespace-nowrap">Movies</span>
-            </Link>
-         </li>
-         <li>
-            <Link to="/dashboard/rooms-admin" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-               <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                  <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z"/>
-               </svg>
-               <span className="flex-1 ms-3 whitespace-nowrap">Rooms</span>
-            </Link>
-         </li>
-         <li>
-            <Link to="/dashboard/sessions-admin" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-               <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                  <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
-               </svg>
-               <span className="flex-1 ms-3 whitespace-nowrap">Sessions</span>
-            </Link>
-         </li>
-         <li>
-            <Link to="reservations-admin" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-               <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
-                  <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z"/>
-               </svg>
-               <span className="flex-1 ms-3 whitespace-nowrap">Reservations</span>
-            </Link>
-         </li>
-        
-         <li>
-              
-               <button 
-                    className="bg-red-500 text-white font-bold py-2 px-4 rounded hover:bg-red-600 transition duration-300" 
-                    onClick={handleLogout}
-               >
-                 Logout
-               </button>          
-         </li>
-      </ul>
-   </div>
+    <>
+      <div className="md:hidden p-4 fixed top-0 left-0 z-50">
+        <button onClick={toggleSidebar} className="text-white text-3xl">
+          {isOpen ? <FiX /> : <FiMenu />}
+        </button>
+      </div>
 
+      <div
+        className={`fixed top-0 left-0 z-40 h-full p-6 bg-[#181823] text-white w-64 transition-transform duration-300 ease-in-out ${
+          isOpen ? "translate-x-0" : "-translate-x-full"
+        } md:translate-x-0 md:static`}
+      >
+        <ul className="space-y-4 font-medium">
+          <li>
+            <Link
+              to="/dashboard"
+              className="flex items-center p-2 rounded-lg hover:bg-gray-700 transition"
+              onClick={handleLinkClick}
+            >
+              <FaHome className="text-xl" />
+              <span className="ms-3">Dashboard</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/dashboard/movise-admin"
+              className="flex items-center p-2 rounded-lg hover:bg-gray-700 transition"
+              onClick={handleLinkClick}
+            >
+              <FaFilm className="text-xl" />
+              <span className="ms-3">Movies</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/dashboard/rooms-admin"
+              className="flex items-center p-2 rounded-lg hover:bg-gray-700 transition"
+              onClick={handleLinkClick}
+            >
+              <FaChair className="text-xl" />
+              <span className="ms-3">Rooms</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/dashboard/sessions-admin"
+              className="flex items-center p-2 rounded-lg hover:bg-gray-700 transition"
+              onClick={handleLinkClick}
+            >
+              <FaRegCalendarAlt className="text-xl" />
+              <span className="ms-3">Sessions</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/dashboard/reservations-admin"
+              className="flex items-center p-2 rounded-lg hover:bg-gray-700 transition"
+              onClick={handleLinkClick}
+            >
+              <FaDoorOpen className="text-xl" />
+              <span className="ms-3">Reservations</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/dashboard/admins-admin"
+              className="flex items-center p-2 rounded-lg hover:bg-gray-700 transition"
+              onClick={handleLinkClick}
+            >
+              <FaUsers className="text-xl" />
+              <span className="ms-3">Admins</span>
+            </Link>
+          </li>
+          <li>
+            <button
+              className="bg-red-500 text-white font-bold py-2 px-4 rounded hover:bg-red-600 transition duration-300"
+              onClick={handleLogout}
+            >
+              Logout
+            </button>
+          </li>
+        </ul>
+      </div>
 
-   
-  )
+      {isOpen && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 z-30 md:hidden"
+          onClick={toggleSidebar}
+        ></div>
+      )}
+    </>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
