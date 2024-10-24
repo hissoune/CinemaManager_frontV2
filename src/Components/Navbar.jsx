e2a370609e581ed4200e18341f2b3f9ff2b5abb1
hissoune <khalidhissoune962@gmail.com>
2024-10-24 11:48:34 +0100

    Add : add the path of the reservations to the nav

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -82,6 +82,11 @@ const Navbar = () => {
           <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border  rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  dark:border-gray-700">
             <li><Link to="/movies-client" className="block py-2 px-3 text-xl text-gray-900 rounded hover:bg-[#C23C39] md:hover:bg-transparent  dark:text-white dark:hover:bg-[#C23C39] ">Movies</Link></li> 
             <li><Link to="/sessions-Client" className="block py-2 px-3 text-xl text-gray-900 rounded hover:bg-[#C23C39] md:hover:bg-transparent  dark:text-white dark:hover:bg-[#C23C39]">Sessions</Link></li> 
+            {(user?.role=='client')? 
+            <li><Link to="/reservations-Client" className="block py-2 px-3 text-xl text-gray-900 rounded hover:bg-[#C23C39] md:hover:bg-transparent  dark:text-white dark:hover:bg-[#C23C39]">Reservations</Link></li> 
+            :
+            <></>}
+            
             <li><Link to="/contact" className="block py-2 px-3 text-xl text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 md:dark:hover:text-[#C23C39]">Contact</Link></li>
           </ul>
         </div>
