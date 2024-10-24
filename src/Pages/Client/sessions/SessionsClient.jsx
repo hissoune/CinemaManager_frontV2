0250fd0894ea8133bef0112da8c70798e67f8f90
hissoune <khalidhissoune962@gmail.com>
2024-10-24 09:15:16 +0100

    Fix : fix the responsive of the sessions

--- src/Pages/Client/sessions/SessionsClient.jsx
+++ src/Pages/Client/sessions/SessionsClient.jsx
@@ -10,22 +10,20 @@ export default function SessionsClient() {
     getAllSessions();
   }, []);
 
-   const handelSessionClick = (session) =>
-   {
+  const handelSessionClick = (session) => {
     navigate(`/sessions-Client/${session._id}`, { state: { session } });
-  
-   }
-   
+  };
+
   if (loading) return <>loading . . .</>;
 
   return (
-    <div className="bg-gradient-to-b  min-h-screen flex items-center justify-center">
+    <div className="bg-gradient-to-b from-gray-900 to-gray-700 min-h-screen flex items-center justify-center">
       <div className="grid grid-cols-12 gap-6 w-[90%] my-10">
         {sessions.map((session, index) => (
           <div
             key={index}
-            onClick={()=>handelSessionClick(session)}
-            className="col-span-12 sm:col-span-6 lg:col-span-3 relative bg-cover bg-center bg-no-repeat transition-transform transform hover:scale-105 shadow-2xl rounded-lg h-96"
+            onClick={() => handelSessionClick(session)}
+            className="col-span-12 sm:col-span-6 lg:col-span-3 relative bg-cover bg-center bg-no-repeat transition-transform transform hover:scale-105 shadow-2xl rounded-lg h-[500px] sm:h-[550px] lg:h-[500px] xl:h-[500px]"
             style={{
               backgroundImage: `url('${session.movie.posterImage || '/2405f5d1220d45fef53df0bfe804e104.jpg'}')`,
             }}
@@ -42,7 +40,9 @@ export default function SessionsClient() {
 
               <div className="text-center">
                 <p className="text-gray-300">Showtime:</p>
-                <p className="text-white font-bold text-lg">{new Date(session.dateTime).toLocaleString()}</p>
+                <p className="text-white font-bold text-lg">
+                  {new Date(session.dateTime).toLocaleString()}
+                </p>
               </div>
             </div>
           </div>
